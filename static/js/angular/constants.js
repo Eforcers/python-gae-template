dbad13fe3536016ad45ea571f1fa829ee8e6b2af
pox <pox@gmail.com>
2016-12-29 22:36:05 -0500

    normal ID

--- static/js/angular/constants.js
+++ static/js/angular/constants.js
@@ -1,3 +1,3 @@
 // var CLIENT_ID = "233923764866.apps.googleusercontent.com";
-var CLIENT_ID = "448548789014-su3rs3853if9vabnpuinr6d4nmku2log.apps.googleusercontent.com";
+var CLIENT_ID = "233923764866.apps.googleusercontent.com";
 var SCOPES = "https://www.googleapis.com/auth/userinfo.email"
